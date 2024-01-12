4c25f92a5ab72590fd198ef79d38f25db32c9bd6
brittanyshelley <brittany.j.shelley@gmail.com>
2024-01-12 14:00:05 -0700

    added styling to photolistitem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -7,11 +7,11 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({ photo }) => {
   const { imageSource, profile, username, location } = photo;
   return (
-    <div>
-      <img src={imageSource} alt="photo" />
-      <img src={profile} alt="profilephoto" />
-      <p>{username}</p>
-      <p>{location.city}, {location.country}</p>
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={imageSource} alt="photo" />
+      <img className="photo-list__image" src={profile} alt="profilephoto" />
+      <p className="photo-list__user-info">{username}</p>
+      <p className="photo-list__user-location">{location.city}, {location.country}</p>
     </div>
   );
 
