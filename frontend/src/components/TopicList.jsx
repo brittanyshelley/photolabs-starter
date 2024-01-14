d39d456f5f1e7a6e8713313b9705002ebdbc7e2d
brittanyshelley <brittany.j.shelley@gmail.com>
2024-01-13 19:27:23 -0700

    Refactor TopicList component and update mock data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,33 +2,34 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
+import topics from "../mocks/topics.js";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+// const sampleDataForTopicList = [
+//   {
+//     id: "1",
+//     slug: "topic-1",
+//     title: "Nature",
+//   },
+//   {
+//     id: "2",
+//     slug: "topic-2",
+//     title: "Travel",
+//   },
+//   {
+//     id: "3",
+//     slug: "topic-3",
+//     title: "People",
+//   },
+// ];
 
 const TopicList = () => {
   // const topics = sampleDataForTopicList.map((topic, index) => {
   // return <TopicListItem key={index} topic={topic} />});
-  const topics = sampleDataForTopicList.map((topic, index) => <TopicListItem key={index} topic={topic} />);
+  const topicList = topics.map((topic, index) => <TopicListItem key={index} topic={topic} />);
 
   return (
     <div className="top-nav-bar__topic-list">
-      {topics}
+      {topicList}
     </div>
   );
 };
