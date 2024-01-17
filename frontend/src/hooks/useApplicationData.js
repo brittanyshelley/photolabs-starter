348e4b1dc3f6fa76bd83899d85712dad1d8fc7d8
brittanyshelley <brittany.j.shelley@gmail.com>
2024-01-17 14:17:04 -0700

    Refactor useApplicationData.js to use useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,8 +1,35 @@
-import { useState } from 'react';
+import { useReducer, useEffect } from 'react';
+
+
+export const ACTIONS = {
+  UPDATE_PHOTO_FAV: 'UPDATE_PHOTO_FAV',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  CLOSE_MODAL: 'CLOSE_MODAL'
+};
+
+
+
+const reducer = (state, action) => {
+  switch (action.type) {
+  case ACTIONS.UPDATE_PHOTO_FAV:
+    return ({ ...state, likes: action.data });
+  case ACTIONS.SELECT_PHOTO:
+    return ({ ...state, activePhoto: action.data });
+  case ACTIONS.CLOSE_MODAL:
+    return ({ ...state, displayModal: !state.displayModal });
+  default:
+    throw new Error(
+      `Tried to reduce with unsupported action type: ${action.type}`
+    );
+  }
+};
 
 const useApplicationData = () => {
 
-  const [state, setState] = useState({
+  const [state, dispatch] = useReducer(reducer, {
 
     likes: [],
     displayModal: false,
@@ -11,27 +38,75 @@ const useApplicationData = () => {
   });
 
   const setActivePhoto = (data) => {
-    setState({ ...state, activePhoto: data });
+    dispatch({ type: ACTIONS.SELECT_PHOTO, data: data });
   };
 
-  const setDisplayModal = (data) => {
-    setState({ ...state, displayModal: data });
-  };
   const setLikes = (data) => {
-    setState({ ...state, likes: data });
+    dispatch({ type: ACTIONS.UPDATE_PHOTO_FAV, data: data });
   };
 
   const toggleModal = () => {
-    setState({ ...state, displayModal: !state.displayModal });
+    dispatch({ type: ACTIONS.CLOSE_MODAL });
   };
-  
+
   return {
     state: state,
     onPhotoSelect: setActivePhoto,
     updateToFavPhotoIds: setLikes,
-    onLoadTopic: null,
+    // onLoadTopic: setTopic,
     onClosePhotoDetailsModal: toggleModal
   };
 };
 
 export default useApplicationData;
+
+
+
+
+
+
+
+// const initialState = {
+//   photos: [],
+//   topics: [],
+//   likes: {},
+//   photo: null,
+//   topic: null,
+//   favPhoto: false
+// };
+// const [state, dispatch] = useReducer(reducer, initialState);
+
+// useEffect(() => {
+//   axios.get('http://localhost:8001/api/topics')
+//     .then(res => {
+//       dispatch({ type: ACTIONS.SET_TOPIC_DATA, payload: res.data });
+//     })
+//     .catch(error => {
+//       console.error('Error fetching topics:', error);
+//     });
+// }, []);
+
+// useEffect(() => {
+//   if (state.topic) {
+//     axios.get(`http://localhost:8001/api/topics/photos/${state.topic}`)
+//       .then(res => {
+//         dispatch({ type: ACTIONS.GET_PHOTOS_BY_TOPICS, payload: res.data });
+//       })
+//       .catch(error => {
+//         console.error('Error fetching photos by topic:', error);
+//       });
+//   } else {
+//     axios.get('http://localhost:8001/api/photos')
+//       .then(res => {
+//         dispatch({ type: ACTIONS.SET_PHOTO_DATA, payload: res.data });
+//       })
+//       .catch(error => {
+//         console.error('Error fetching photos:', error);
+//       });
+//   }
+// }, [state.topic]);
+
+// return { state, dispatch, ACTIONS };
+// }
+
+// export default useApplicationData;
